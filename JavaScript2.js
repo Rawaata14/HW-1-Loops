827327023da3f49e10fb77c72cdfbe4237c13a56
Rawaata14 <rawatareef1@gmail.com>
2025-04-11 11:37:30 +0300

    Task2

--- JavaScript2.js
+++ JavaScript2.js
@@ -1,8 +1,15 @@
-let i = 1;
-let sum = 0;
-while(i <= 100)
-{
-    sum = sum + i;
-    i++;
-}
-console.log("The sum is: ",sum);
+// let i = 1;
+// let sum = 0;
+// while(i <= 100)
+// {
+//     sum = sum + i;
+//     i++;
+// }
+// console.log("The sum is: ",sum);
+
+//Task#2
+let number;
+do {
+    number = prompt("Enter a number");
+} while (isNaN(number) || number <= 0);
+console.log("The number is: ", number);
